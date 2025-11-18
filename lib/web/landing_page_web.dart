cc4e79fe4102985b0806ac15314cfa7b1ec79556
emmanueluwa <oemmanuel.uwadiae@gmail.com>
2025-11-18 20:16:04 +0000

    adding about me section

--- lib/web/landing_page_web.dart
+++ lib/web/landing_page_web.dart
@@ -64,15 +64,32 @@ class _LandingPageWebState extends State<LandingPageWeb> {
                       ),
                       child: const SansBold("Hello I'm", 15.0),
                     ),
-                    SizedBox(height: 15.0),
-                    SansBold("Osasu Uwadiae", 55.0),
-                    Sans("Flutter developer", 30.0),
-                    SizedBox(height: 15.0),
-                    Row(
+                    const SizedBox(height: 15.0),
+                    const SansBold("Osasu Uwadiae", 55.0),
+                    const Sans("Flutter developer", 30.0),
+                    const SizedBox(height: 15.0),
+                    const Row(
                       children: [
                         Icon(Icons.email),
                         SizedBox(width: 20.0),
                         Sans("osasu@fulodev.com", 15.0),
+                      ],
+                    ),
+                    const SizedBox(height: 10.0),
+
+                    // const Row(
+                    //   children: [
+                    //     Icon(Icons.call),
+                    //     SizedBox(width: 20.0),
+                    //     Sans("+44", 15.0),
+                    //   ],
+                    // ),
+                    // const SizedBox(height: 10.0),
+                    const Row(
+                      children: [
+                        Icon(Icons.location_pin),
+                        SizedBox(width: 20.0),
+                        Sans("London", 15.0),
                       ],
                     ),
                   ],
@@ -91,6 +108,96 @@ class _LandingPageWebState extends State<LandingPageWeb> {
                       backgroundColor: Colors.white,
                     ),
                   ),
+                ),
+              ],
+            ),
+          ),
+
+          //second page
+          Container(
+            height: heightDevice / 1.5,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Image.asset("assets/web.jpg", height: heightDevice / 1.7),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SansBold("About me", 40.0),
+                    SizedBox(height: 15.0),
+                    Sans(
+                      "Hello! I'm Osasu I specialise in flutter development",
+                      15.0,
+                    ),
+                    Sans(
+                      "I strive to ensure astounding peformance with state of ",
+                      15.0,
+                    ),
+                    Sans(
+                      "the art security for Androd, IOS, Web, Mac, Linux and Windows.",
+                      15.0,
+                    ),
+
+                    SizedBox(height: 10),
+
+                    Row(
+                      children: [
+                        Container(
+                          decoration: BoxDecoration(
+                            border: Border.all(
+                              color: Colors.redAccent,
+                              style: BorderStyle.solid,
+                              width: 2.0,
+                            ),
+                            borderRadius: BorderRadius.circular(5.0),
+                          ),
+                          padding: EdgeInsets.all(7.0),
+                          child: Sans("Flutter", 15.0),
+                        ),
+                        SizedBox(width: 7),
+                        Container(
+                          decoration: BoxDecoration(
+                            border: Border.all(
+                              color: Colors.redAccent,
+                              style: BorderStyle.solid,
+                              width: 2.0,
+                            ),
+                            borderRadius: BorderRadius.circular(5.0),
+                          ),
+                          padding: EdgeInsets.all(7.0),
+                          child: Sans("Python", 15.0),
+                        ),
+                        SizedBox(width: 7),
+                        Container(
+                          decoration: BoxDecoration(
+                            border: Border.all(
+                              color: Colors.redAccent,
+                              style: BorderStyle.solid,
+                              width: 2.0,
+                            ),
+                            borderRadius: BorderRadius.circular(5.0),
+                          ),
+                          padding: EdgeInsets.all(7.0),
+                          child: Sans("FastAPI", 15.0),
+                        ),
+                        SizedBox(width: 7),
+                        Container(
+                          decoration: BoxDecoration(
+                            border: Border.all(
+                              color: Colors.redAccent,
+                              style: BorderStyle.solid,
+                              width: 2.0,
+                            ),
+                            borderRadius: BorderRadius.circular(5.0),
+                          ),
+                          padding: EdgeInsets.all(7.0),
+                          child: Sans("AI", 15.0),
+                        ),
+                      ],
+                    ),
+                  ],
                 ),
               ],
             ),
