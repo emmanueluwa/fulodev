36e8c7f61efd9eb471e427461c52f4ac623c56db
emmanueluwa <oemmanuel.uwadiae@gmail.com>
2025-11-18 11:02:22 +0000

    adding header and image for web

--- lib/components.dart
+++ lib/components.dart
@@ -1,1 +1,49 @@
 //file for widgets used throughout the app
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class TabsWeb extends StatefulWidget {
+  final title;
+
+  const TabsWeb(this.title, {super.key});
+
+  @override
+  State<TabsWeb> createState() => _TabsWebState();
+}
+
+class _TabsWebState extends State<TabsWeb> {
+  bool isSelected = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return MouseRegion(
+      onEnter: (_) {
+        setState(() {
+          isSelected = true;
+        });
+      },
+
+      onExit: (_) {
+        setState(() {
+          isSelected = false;
+        });
+      },
+
+      child: AnimatedDefaultTextStyle(
+        duration: const Duration(milliseconds: 100),
+        curve: Curves.elasticIn,
+        style: isSelected
+            ? GoogleFonts.archivoBlack(
+                shadows: [Shadow(color: Colors.black, offset: Offset(0, -8))],
+                color: Colors.transparent,
+                fontSize: 25.0,
+                decoration: TextDecoration.underline,
+                decorationThickness: 2,
+                decorationColor: Colors.redAccent,
+              )
+            : GoogleFonts.archivoBlack(color: Colors.black, fontSize: 23.0),
+        child: Text(widget.title),
+      ),
+    );
+  }
+}
--- lib/web/landing_page_web.dart
+++ lib/web/landing_page_web.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fulodev/components.dart';
 
 class LandingPageWeb extends StatefulWidget {
   const LandingPageWeb({super.key});
@@ -10,6 +11,42 @@ class LandingPageWeb extends StatefulWidget {
 class _LandingPageWebState extends State<LandingPageWeb> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      drawer: Drawer(),
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0.0,
+        iconTheme: IconThemeData(size: 25.0, color: Colors.black),
+        title: Row(
+          children: [
+            Spacer(flex: 3),
+            TabsWeb("Home"),
+            Spacer(),
+            TabsWeb("Projects"),
+            Spacer(),
+            TabsWeb("Blog"),
+            Spacer(),
+            TabsWeb("About"),
+            Spacer(),
+            TabsWeb("Contact"),
+            Spacer(),
+          ],
+        ),
+      ),
+      body: CircleAvatar(
+        radius: 147.0,
+        backgroundColor: Colors.redAccent,
+        child: CircleAvatar(
+          radius: 143.0,
+          backgroundColor: Colors.black,
+          child: CircleAvatar(
+            radius: 140.0,
+            backgroundImage: AssetImage("assets/draft_portfolio_image.png"),
+            backgroundColor: Colors.white,
+          ),
+        ),
+      ),
+    );
   }
 }
