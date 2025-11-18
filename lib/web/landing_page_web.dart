7c4d9e5d3f8e504c2fe111f687d9aaf78cdb2173
emmanueluwa <oemmanuel.uwadiae@gmail.com>
2025-11-18 16:28:33 +0000

    adding intro section

--- lib/web/landing_page_web.dart
+++ lib/web/landing_page_web.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:fulodev/components.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class LandingPageWeb extends StatefulWidget {
   const LandingPageWeb({super.key});
@@ -11,6 +12,8 @@ class LandingPageWeb extends StatefulWidget {
 class _LandingPageWebState extends State<LandingPageWeb> {
   @override
   Widget build(BuildContext context) {
+    var heightDevice = MediaQuery.of(context).size.height;
+
     return Scaffold(
       drawer: Drawer(),
       backgroundColor: Colors.white,
@@ -34,18 +37,65 @@ class _LandingPageWebState extends State<LandingPageWeb> {
           ],
         ),
       ),
-      body: CircleAvatar(
-        radius: 147.0,
-        backgroundColor: Colors.redAccent,
-        child: CircleAvatar(
-          radius: 143.0,
-          backgroundColor: Colors.black,
-          child: CircleAvatar(
-            radius: 140.0,
-            backgroundImage: AssetImage("assets/draft_portfolio_image.png"),
-            backgroundColor: Colors.white,
+      body: ListView(
+        children: [
+          //first page
+          Container(
+            height: heightDevice - 56,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      decoration: const BoxDecoration(
+                        color: Colors.redAccent,
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(20.0),
+                          topRight: Radius.circular(20.0),
+                          bottomRight: Radius.circular(20.0),
+                        ),
+                      ),
+                      padding: const EdgeInsets.symmetric(
+                        vertical: 10.0,
+                        horizontal: 20.0,
+                      ),
+                      child: const SansBold("Hello I'm", 15.0),
+                    ),
+                    SizedBox(height: 15.0),
+                    SansBold("Osasu Uwadiae", 55.0),
+                    Sans("Flutter developer", 30.0),
+                    SizedBox(height: 15.0),
+                    Row(
+                      children: [
+                        Icon(Icons.email),
+                        SizedBox(width: 20.0),
+                        Sans("osasu@fulodev.com", 15.0),
+                      ],
+                    ),
+                  ],
+                ),
+                const CircleAvatar(
+                  radius: 147.0,
+                  backgroundColor: Colors.redAccent,
+                  child: CircleAvatar(
+                    radius: 143.0,
+                    backgroundColor: Colors.black,
+                    child: CircleAvatar(
+                      radius: 140.0,
+                      backgroundImage: AssetImage(
+                        "assets/draft_portfolio_image.png",
+                      ),
+                      backgroundColor: Colors.white,
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
