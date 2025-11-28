847227cf548257efc19ac5f2ff8c7b8d3291bf34
emmanueluwa <oemmanuel.uwadiae@gmail.com>
2025-11-28 08:14:09 +0000

    adding tabs to mobile menu

--- lib/components.dart
+++ lib/components.dart
@@ -51,6 +51,34 @@ class _TabsWebState extends State<TabsWeb> {
   }
 }
 
+class TabsMobile extends StatefulWidget {
+  final text;
+  final route;
+
+  const TabsMobile({super.key, @required this.text, @required this.route});
+
+  @override
+  State<TabsMobile> createState() => _TabsMobileState();
+}
+
+class _TabsMobileState extends State<TabsMobile> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialButton(
+      elevation: 20.0,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5.0)),
+      height: 50.0,
+      minWidth: 200.0,
+      color: Colors.black,
+      child: Text(
+        widget.text,
+        style: GoogleFonts.openSans(fontSize: 20.0, color: Colors.white),
+      ),
+      onPressed: () {},
+    );
+  }
+}
+
 class SansBold extends StatelessWidget {
   final text;
   final size;
--- lib/mobile/landing_page_mobile.dart
+++ lib/mobile/landing_page_mobile.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fulodev/components.dart';
 
 class LandingPageMobile extends StatefulWidget {
   const LandingPageMobile({super.key});
@@ -10,6 +11,41 @@ class LandingPageMobile extends StatefulWidget {
 class _LandingPageMobileState extends State<LandingPageMobile> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    var widthDevice = MediaQuery.of(context).size.width;
+
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0.0,
+        iconTheme: IconThemeData(size: 35.0, color: Colors.black),
+      ),
+      endDrawer: Drawer(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            DrawerHeader(
+              padding: EdgeInsets.only(bottom: 20.0),
+              child: Container(
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  border: Border.all(width: 2.0, color: Colors.black),
+                ),
+                child: Image.asset("assets/draft_portfolio_image.png"),
+              ),
+            ),
+            TabsMobile(text: "Home", route: "/"),
+            SizedBox(height: 20.0),
+            TabsMobile(text: "Works", route: "/works"),
+            SizedBox(height: 20.0),
+            TabsMobile(text: "Blog", route: "/blog"),
+            SizedBox(height: 20.0),
+            TabsMobile(text: "About", route: "/about"),
+            SizedBox(height: 20.0),
+            TabsMobile(text: "Contact", route: "/contact"),
+          ],
+        ),
+      ),
+    );
   }
 }
