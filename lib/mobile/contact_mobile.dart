8b9fb7f2b2b8f7d7413e11f2eb0b141c34795223
emmanueluwa <oemmanuel.uwadiae@gmail.com>
2025-11-30 07:54:45 +0000

    adding drawer navigation to contact mobile screen

--- lib/mobile/contact_mobile.dart
+++ lib/mobile/contact_mobile.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:fulodev/components.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class ContactMobile extends StatefulWidget {
   const ContactMobile({super.key});
@@ -10,6 +13,80 @@ class ContactMobile extends StatefulWidget {
 class _ContactMobileState extends State<ContactMobile> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      backgroundColor: Colors.white,
+      endDrawer: Drawer(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            DrawerHeader(
+              padding: EdgeInsets.only(bottom: 20.0),
+              child: Container(
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  border: Border.all(width: 2.0, color: Colors.black),
+                ),
+                child: Image.asset("assets/draft_portfolio_image.png"),
+              ),
+            ),
+            TabsMobile(text: "Home", route: "/"),
+            SizedBox(height: 20.0),
+            TabsMobile(text: "Works", route: "/works"),
+            SizedBox(height: 20.0),
+            TabsMobile(text: "Blog", route: "/blog"),
+            SizedBox(height: 20.0),
+            TabsMobile(text: "About", route: "/about"),
+            SizedBox(height: 20.0),
+            TabsMobile(text: "Contact", route: "/contact"),
+            SizedBox(height: 20.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                IconButton(
+                  onPressed: () async =>
+                      await launchUrl(Uri.parse("https://www.instagram.com/")),
+                  icon: SvgPicture.asset(
+                    "assets/instagram.svg",
+                    color: Colors.black,
+                    width: 35.0,
+                  ),
+                ),
+                IconButton(
+                  onPressed: () async =>
+                      await launchUrl(Uri.parse("https://www.twitter.com/")),
+                  icon: SvgPicture.asset(
+                    "assets/twitter.svg",
+                    color: Colors.black,
+                    width: 35.0,
+                  ),
+                ),
+                IconButton(
+                  onPressed: () async => await launchUrl(
+                    Uri.parse("https://www.github.com/emmanueluwa"),
+                  ),
+                  icon: SvgPicture.asset(
+                    "assets/github.svg",
+                    color: Colors.black,
+                    width: 35.0,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+      body: NestedScrollView(
+        headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+          return <Widget>[
+            SliverAppBar(
+              backgroundColor: Colors.white,
+              iconTheme: IconThemeData(size: 35.0, color: Colors.black),
+            ),
+          ];
+        },
+        body: SingleChildScrollView(),
+      ),
+    );
   }
 }
