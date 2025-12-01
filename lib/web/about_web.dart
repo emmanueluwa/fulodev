bf6f52a87f02dfe5bbee05428b4955fadb5db607
emmanueluwa <oemmanuel.uwadiae@gmail.com>
2025-12-01 08:47:30 +0000

    completing about page for web

--- lib/web/about_web.dart
+++ lib/web/about_web.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:fulodev/components.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class AboutWeb extends StatefulWidget {
   const AboutWeb({super.key});
@@ -10,6 +14,232 @@ class AboutWeb extends StatefulWidget {
 class _AboutWebState extends State<AboutWeb> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    urlLauncher(String imgPath, String url) {
+      return IconButton(
+        icon: SvgPicture.asset(imgPath, color: Colors.black, width: 35.0),
+        onPressed: () async {
+          await launchUrl(Uri.parse(url));
+        },
+      );
+    }
+
+    redContainer(String text) {
+      return Container(
+        decoration: BoxDecoration(
+          border: Border.all(
+            color: Colors.redAccent,
+            style: BorderStyle.solid,
+            width: 2.0,
+          ),
+          borderRadius: BorderRadius.circular(5.0),
+        ),
+        padding: EdgeInsets.all(7.0),
+        child: Text(text, style: GoogleFonts.openSans(fontSize: 15.0)),
+      );
+    }
+
+    var widthDevice = MediaQuery.of(context).size.width;
+
+    return Scaffold(
+      drawer: Drawer(
+        backgroundColor: Colors.white,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const CircleAvatar(
+              radius: 72.0,
+              backgroundColor: Colors.redAccent,
+              child: CircleAvatar(
+                radius: 70.0,
+                backgroundColor: Colors.white,
+                backgroundImage: AssetImage("assets/draft_portfolio_image.png"),
+              ),
+            ),
+            const SizedBox(height: 15.0),
+            const SansBold("Osasu Uwadiae", 30.0),
+            const SizedBox(height: 15.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                urlLauncher(
+                  "assets/instagram.svg",
+                  "https://www.instagram.com/",
+                ),
+                urlLauncher("assets/twitter.svg", "https://x.com/fuloXXXX/"),
+                urlLauncher(
+                  "assets/github.svg",
+                  "https://github.com/emmanueluwa/",
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0.0,
+        iconTheme: const IconThemeData(size: 25.0, color: Colors.black),
+        title: const Row(
+          children: [
+            Spacer(flex: 3),
+            TabsWeb(title: "Home", route: "/"),
+            Spacer(),
+            TabsWeb(title: "Projects", route: "/projects"),
+            Spacer(),
+            TabsWeb(title: "Blog", route: "/blog"),
+            Spacer(),
+            TabsWeb(title: "About", route: "/about"),
+            Spacer(),
+            TabsWeb(title: "Contact", route: "/contact"),
+            Spacer(),
+          ],
+        ),
+      ),
+      body: ListView(
+        children: [
+          //About me, first section
+          SizedBox(
+            height: 500.0,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SansBold("About me", 40.0),
+                    SizedBox(height: 15.0),
+                    Sans(
+                      "Hello! I'm Osasu Uwadiae I specialize in flutter development. ",
+                      15.0,
+                    ),
+                    Sans(
+                      "I strive to ensure astounding erformance with state of ",
+                      15.0,
+                    ),
+                    Sans(
+                      "the art security for android,  IOS, Web, Mac, Linux and Windows",
+                      15.0,
+                    ),
+                    SizedBox(height: 10.0),
+
+                    Row(
+                      children: [
+                        redContainer("Flutter"),
+                        SizedBox(width: 7.0),
+                        redContainer("FastAPI"),
+                        SizedBox(width: 7.0),
+                        redContainer("Android"),
+                        SizedBox(width: 7.0),
+                        redContainer("IOS"),
+                        SizedBox(width: 7.0),
+                        redContainer("Windows"),
+                      ],
+                    ),
+                  ],
+                ),
+
+                const CircleAvatar(
+                  radius: 147.0,
+                  backgroundColor: Colors.redAccent,
+                  child: CircleAvatar(
+                    radius: 143.0,
+                    backgroundColor: Colors.black,
+                    child: CircleAvatar(
+                      radius: 140.0,
+                      backgroundImage: AssetImage(
+                        "assets/draft_portfolio_image.png",
+                      ),
+                      backgroundColor: Colors.white,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+
+          // web development, second section
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              AnimatedCard(
+                imagePath: "assets/webL.png",
+                height: 250.0,
+                width: 250.0,
+              ),
+              SizedBox(
+                width: widthDevice / 3,
+                child: Column(
+                  children: [
+                    SansBold("Web development", 30.0),
+                    SizedBox(height: 15.0),
+                    Sans(
+                      "I'm here to build your presence online with state of the art web applications.",
+                      15.0,
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 20.0),
+
+          // app development, third section
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              SizedBox(
+                width: widthDevice / 3,
+                child: Column(
+                  children: [
+                    SansBold("App development", 30.0),
+                    SizedBox(height: 15.0),
+                    Sans(
+                      "Do you need a high-performance, responsive and beautiful app? Don't worry, I've got you covered.",
+                      15.0,
+                    ),
+                  ],
+                ),
+              ),
+              AnimatedCard(
+                imagePath: "assets/app.png",
+                height: 250.0,
+                width: 250.0,
+                reverse: true,
+              ),
+            ],
+          ),
+          SizedBox(height: 20.0),
+
+          //backend development, fourth section
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              AnimatedCard(
+                imagePath: "assets/python.png",
+                height: 250.0,
+                width: 250.0,
+              ),
+              SizedBox(
+                width: widthDevice / 3,
+                child: Column(
+                  children: [
+                    SansBold("Backend development", 30.0),
+                    SizedBox(height: 15.0),
+                    Sans(
+                      "Do you need a scalable and secure backend? Let's have a conversation about how I can help you with that.",
+                      15.0,
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 40.0),
+        ],
+      ),
+    );
   }
 }
