ca966ff988c16e4ead4b5e3c7a597a9d2a607524
emmanueluwa <oemmanuel.uwadiae@gmail.com>
2025-12-03 05:53:39 +0000

    adding works mobile screen

--- lib/mobile/works_mobile.dart
+++ lib/mobile/works_mobile.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:fulodev/components.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class WorksMobile extends StatefulWidget {
   const WorksMobile({super.key});
@@ -10,6 +13,122 @@ class WorksMobile extends StatefulWidget {
 class _WorksMobileState extends State<WorksMobile> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SafeArea(
+      child: Scaffold(
+        extendBodyBehindAppBar: true,
+        backgroundColor: Colors.white,
+        endDrawer: Drawer(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              DrawerHeader(
+                padding: EdgeInsets.only(bottom: 20.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    border: Border.all(width: 2.0, color: Colors.black),
+                  ),
+                  child: Image.asset(
+                    "assets/draft_portfolio_image.png",
+                    filterQuality: FilterQuality.high,
+                  ),
+                ),
+              ),
+              TabsMobile(text: "Home", route: "/"),
+              SizedBox(height: 20.0),
+              TabsMobile(text: "Works", route: "/works"),
+              SizedBox(height: 20.0),
+              TabsMobile(text: "Blog", route: "/blog"),
+              SizedBox(height: 20.0),
+              TabsMobile(text: "About", route: "/about"),
+              SizedBox(height: 20.0),
+              TabsMobile(text: "Contact", route: "/contact"),
+              SizedBox(height: 20.0),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  IconButton(
+                    onPressed: () async => await launchUrl(
+                      Uri.parse("https://www.instagram.com/"),
+                    ),
+                    icon: SvgPicture.asset(
+                      "assets/instagram.svg",
+                      color: Colors.black,
+                      width: 35.0,
+                    ),
+                  ),
+                  IconButton(
+                    onPressed: () async =>
+                        await launchUrl(Uri.parse("https://www.twitter.com/")),
+                    icon: SvgPicture.asset(
+                      "assets/twitter.svg",
+                      color: Colors.black,
+                      width: 35.0,
+                    ),
+                  ),
+                  IconButton(
+                    onPressed: () async => await launchUrl(
+                      Uri.parse("https://www.github.com/emmanueluwa"),
+                    ),
+                    icon: SvgPicture.asset(
+                      "assets/github.svg",
+                      color: Colors.black,
+                      width: 35.0,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+        body: NestedScrollView(
+          headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+            return <Widget>[
+              SliverAppBar(
+                expandedHeight: 400.0,
+                backgroundColor: Colors.white,
+                iconTheme: IconThemeData(size: 35.0, color: Colors.black),
+                flexibleSpace: FlexibleSpaceBar(
+                  background: Image.asset(
+                    "assets/works.jpg",
+                    fit: BoxFit.cover,
+                    filterQuality: FilterQuality.high,
+                  ),
+                ),
+              ),
+            ];
+          },
+          body: ListView(
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  SizedBox(height: 20.0),
+                  SansBold("Works", 35.0),
+                  SizedBox(height: 20.0),
+                  AnimatedCard(
+                    imagePath: "assets/portfolio_screenshot.png",
+                    fit: BoxFit.contain,
+                    height: 150.0,
+                    width: 300.0,
+                  ),
+                  SizedBox(height: 30.0),
+
+                  Sans("Portfolio", 20.0),
+                  SizedBox(height: 10.0),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                    child: Sans(
+                      "Deployed on IOS, Android and Web, this portfolio project was built with flutter for the frontend and responsive UI along with FastAPI for the backend.",
+                      15.0,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
